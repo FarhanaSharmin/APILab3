e10f7b725f5ac8b7e7e0a997f532c7d28d4c1e86
georgelele1 <43303561+georgelele1@users.noreply.github.com>
2019-10-22 22:37:35 -0400

    Add files via upload

---
+++ 300873342(Farhana)_300895511(Yanbo)_Lab3/300873342(Farhana)_300895511(Yanbo)_Lab3/Data/DynamoDBServicesContext.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Global.Data
+{
+    public class DynamoDBServicesContext
+    {
+        IAmazonDynamoDB dynamoDBClient { get; set; }
+
+        public DynamoDBServicesContext(IAmazonDynamoDB dynamoDBClient)
+        {
+            this.dynamoDBClient = dynamoDBClient;
+        }
+    }
+}
---
+++ 300873342(Farhana)_300895511(Yanbo)_Lab3/300873342(Farhana)_300895511(Yanbo)_Lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _300873342_Farhana__300895511_Yanbo__Lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
